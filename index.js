6afca47da01527337edb833594cd6c2b114b424f
hihei56 <95565742+hihei56@users.noreply.github.com>
2026-07-18 14:23:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ async function updatePresence() {
           },
           buttons: ['サーバーに参加'],
           metadata: {
-            button_urls: ['https://discord.gg/RcYbNZTQcn']
+            button_urls: ['https://discord.gg/M34RA8u2gZ']
           }
         }]
       }
